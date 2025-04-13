5dce193acbcb87653b36b9b50a0555a0ca511de7
Martin Piñero <martinpiniero@hotmail.com>
2025-04-13 19:32:47 -0300

    version inicial

---
+++ src/ar/edu/unq/poo2/tp0/punto.java
@@ -0,0 +1,39 @@
+package ar.edu.unq.poo2.tp0;
+
+public class punto {
+	
+	private float x;
+	private float y;
+	
+	
+	
+	public float getX() {
+		return x;
+	}
+
+
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+
+
+	public float getY() {
+		return y;
+	}
+
+
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+
+
+	public punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+}
