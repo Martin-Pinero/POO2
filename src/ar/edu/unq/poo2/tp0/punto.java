9128a6c317f829827b557ea18778126287bd6d1d
Martin Piñero <martinpiniero@hotmail.com>
2025-04-13 19:37:01 -0300

    modificado settings, se refactoriza el constructor de punto

--- src/ar/edu/unq/poo2/tp0/punto.java
+++ src/ar/edu/unq/poo2/tp0/punto.java
@@ -6,34 +6,28 @@ public class punto {
 	private float y;
 	
 	
-	
+	public void setXY(float x, float y) {
+		this.setX(x);
+		this.setY(y);
+	}
 	public float getX() {
 		return x;
 	}
-
-
 
 	public void setX(float x) {
 		this.x = x;
 	}
 
-
-
 	public float getY() {
 		return y;
 	}
-
-
 
 	public void setY(float y) {
 		this.y = y;
 	}
 
-
-
 	public punto(float x, float y) {
 		super();
-		this.x = x;
-		this.y = y;
+		this.setXY(x, y);
 	}
 }
